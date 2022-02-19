780cc7954f634d61276c1ca1f248a39752360ee5
Carwi Barrios <carwelsbarrios@gmail.com>
2022-02-19 11:56:59 +0100

    working auth in process signup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const saltRound = 5;
+const bcrypt = require("bcrypt")
+
+const User = require("../models/User.model")
+
+const isLoggedIn = require("..middleware/isLoggedIn.js")
+const isNotLoggedIn = require("..middleware/isLoggedOut.js")
+
+router.get("/signup", isNotLoggedIn, (req, res) => {
+  res.render("auth/signup")
+})
+
+router.post("/signup", isNotLoggedIn, (req, res, next) => {
+  const {username, password, age, name, interests, aboutMe, campus, profileImg} = req.body;
+
+  if(!username || !password || !age || !name || !campus || !profileImg) {
+    return res.render("auth/signup", { errorMessage: "Please provide the mandatory fields"})
+  }
+
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+
+})
+
+
+
 
 module.exports = router;
