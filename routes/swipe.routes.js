145c5c9258ac8f85b48f81b2d8593695442786e1
Diego Yupanqui <diegoyupanquichavarry@gmail.com>
2022-02-19 13:29:58 +0100

    changes

--- routes/swipe.routes.js
+++ routes/swipe.routes.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../models/User.model")
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/show/:userId', (req,res)=>{
+  User.findById(req.params.userId)
+  .then(user => getRandomUser(user))
 });
+
+router.post("/like/:userId/:likedId", (req,res)=>{
+
+})
+
+router.post("/dislike/:userId/:dislikedId",(req,res)=>{
+
+})
+
+router.post("/filter/:userId", (req,res)=>{
+
+})
+
+const getRandomUser = (user)=>{
+  const liked = user.liked;
+  const disliked = user.disliked
+
+  User.findOne({"_id": {$nin: liked}})
+}
 
 module.exports = router;
