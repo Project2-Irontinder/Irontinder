d1f4c3d24a08de9ba622f36c129d917667d80a1c
Diego Yupanqui <diegoyupanquichavarry@gmail.com>
2022-02-19 16:13:59 +0100

    swipe step1

--- routes/swipe.routes.js
+++ routes/swipe.routes.js
@@ -4,28 +4,47 @@ const User = require("../models/User.model")
 
 
 /* GET users listing. */
-router.get('/show/:userId', (req,res)=>{
+router.get('/show/:userId', (req, res) => {
   User.findById(req.params.userId)
-  .then(user => getRandomUser(user))
+    .then(user => {
+      //console.log(random)
+      res.render("pages/swipe", getRandomUser(user))
+    
+    })
 });
 
-router.post("/like/:userId/:likedId", (req,res)=>{
+router.post("/like/:userId/:likedId", (req, res) => {
 
 })
 
-router.post("/dislike/:userId/:dislikedId",(req,res)=>{
+router.post("/dislike/:userId/:dislikedId", (req, res) => {
 
 })
 
-router.post("/filter/:userId", (req,res)=>{
+router.post("/filter/:userId", (req, res) => {
 
 })
 
-const getRandomUser = (user)=>{
+const getRandomUser = (user) => {
   const liked = user.liked;
   const disliked = user.disliked
 
-  return User.findOne({"_id": {$nin: liked}})
+  console.log(liked)
+  console.log(disliked)
+
+  User.find({
+    $and: [
+      { "_id": { $nin: liked } },
+      { "_id": { $nin: disliked } }
+    ]
+  })
+    .populate(liked)
+    .populate(disliked)
+    .then(users => {
+      const randomUser = Math.floor(Math.random() * users.length)
+      //console.log(users[randomUser])
+      return users[randomUser]
+    })
 }
 
 module.exports = router;
