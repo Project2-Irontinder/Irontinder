10747d2d68c06dbd10501846d9546a2fa8e12cdd
Fer Sánchez <sangarfer@gmail.com>
2022-02-19 13:29:47 +0100

    profile routes in progress

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const fileUploader = require("../config/cloudinary.config");
+const User = require("../models/User.model");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post(":userId/delete-photo/:photoId", (req, res)=>{});
+router.post(":userId/add-newPhoto", (req, res)=>{});
+router.post(":userId/edit-imgProfile", fileUploader.single("imgProfile"), (req, res)=>{
+  const id = req.params.id;
+  const imgProfile = req.file.path;
+
+  
+});
+
+router.post(":userId/edit-infoProfile", (req, res)=>{
+  const id = req.params.id;
+  const {name, interests, aboutMe} = req.body;
+  User.findByIdAndUpdate(id, {name, interests, aboutMe})
+  .then(()=>res.redirect(`/profile/${id}`))
+  .catch((err)=>{console.log(err)});
+});
+
+router.get('/:userId', (req, res)=>{
+  const id = req.params.userId;
+
+  if(req.session.userId !== id){
+    User.findById(id)
+    .populate("photos")
+    .then((user)=>{
+      return res.render("pages/profile", {isNotTheOwner});
+    })
+    .catch((err)=>{console.log(err)});
+  }
+
+  User.findById(id)
+  .populate("photos")
+  .then((user)=>{
+    res.render("pages/profile", {isTheOwner});
+  })
+  .catch((err)=>{console.log(err)});
 });
 
 module.exports = router;
